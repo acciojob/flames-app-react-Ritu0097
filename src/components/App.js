fd781cad2ec2b4c1dba60446d60e2ea50967302f
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-20 17:04:20 +0530

    flames

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,78 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
- 
-class App extends Component{
-  state = {
-    data : {name1:'',
-      name2:'',
-      answer:''}
-  }
- 
-  handleChange = (e) => {
-        const data = {...this.state.data}
-        data[e.currentTarget.name] = e.currentTarget.value;
-        this.setState({data})
-  }
- 
-  match = (s1,s2) => {
-      let count = 0
-      let dict = {}
-      for (let i=0;i<s1.length;i++){
-          if(s1[i] in dict){
-              dict[s1[i]]+=1
-          }else{
-              dict[s1[i]] = 1
-          }
-      }
-      for (let j=0;j<s2.length;j++){
-          if(s2[j] in dict){
-              dict[s2[j]]-=1
-              count+=1
-          }
-          if(dict[s2[j]]==0){
-              delete dict[s2[j]]
-          }
-      }
-      return count
-  }
- 
-  handleClick = e => {
-    if(e.currentTarget.name == 'clear'){
-          const data = {
-              name1:'',
-              name2:'',
-              answer:''
-          }
-          this.setState({data})
-          return
-    }else if(this.state.data.name1.length && this.state.data.name2.length){
-        let count = this.state.data.name1.length+this.state.data.name2.length-(this.match(this.state.data.name1,this.state.data.name2)*2)
-        let answer;
-        if(count%6==1){
-            answer = "Friends"
-        }else if(count%6==2){
-            answer = "Love"
-        }else if(count%6==3){
-            answer = "Affection"
-        }else if(count%6==4){
-            answer = "Marriage"
-        }else if(count%6==5){
-            answer = "Enemy"
-        }else{
-            answer = "Siblings"
+
+const App = () => {
+    const [firstname, setFirstname] = useState("");
+    const [secondname, setSecondname] = useState("");
+    const [status, setStatus] = useState("");
+
+    const calculateRelationship = () => {
+        if(firstname === '' || secondname === ''){
+            setStatus('Please Enter valid input');
+            return;
         }
-        const data = {...this.state.data}
-        data.answer = answer
-        this.setState({data})
-    }else{
-        const data = {...this.state.data}
-        data.answer = 'Please Enter valid input'
-        this.setState({data})
+
+        let firstArr = firstname.split("");
+        let secondArr = secondname.split("");
+        let newArr = [];
+
+        // To splice array and modify string
+        for(let i=0; i<firstArr.length; i++){
+            let flag = false;
+            for(let j=0; j<secondArr.length; j++){
+                if(firstArr[i]===secondArr[j]){
+                    secondArr.splice(j, 1);
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                // Then only need to add it(char) in our first string array
+                newArr.push(firstArr[i]);
+            }
+        }
+
+
+        // To calculate relationship status
+        let str = newArr.join("") + secondArr.join("");
+        let n = (str.length) % 6;
+        switch(n){
+            case 1 :
+                return setStatus('Friends');
+            case 2 :
+                return setStatus('Love');
+            case 3 :
+                return setStatus('Affection');
+            case 4 :
+                return setStatus('Marriage');
+            case 5 :
+                return setStatus('Enemy');
+            case 0 :
+                return setStatus('Siblings');
+        }
     }
-  }
- 
-  render(){
-    return(
-        <div>
-          <input data-testid="input1" value={this.state.data.name1} onChange={this.handleChange} type="text" name="name1" placeholder={'Enter first name'}/>
-          <input data-testid="input2" value={this.state.data.name2} onChange={this.handleChange} type="text" name="name2" placeholder={'Enter second name'}/>
-          <button data-testid="calculate_relationship" onClick={this.handleClick} name="calculate_relationship">Calculate Relationship Future</button>
-          <button data-testid="clear" onClick={this.handleClick} name="clear">Clear</button>
-          <h3 data-testid="answer">{this.state.data.answer}</h3>
+
+    const clearStatus = () => {
+        setFirstname('');
+        setSecondname('');
+        setStatus('');
+    }
+
+    return (
+        <div id="main">
+            {/* Do not remove the main div */}
+            <input type="text" placeholder="Enter first name" value={firstname} 
+                onChange={(e) => setFirstname(e.target.value)} data-testid="input1" name="name1"/>
+            <input type="text" placeholder="Enter second name" value={secondname} 
+                onChange={(e) => setSecondname(e.target.value)} data-testid="input2" name="name2" />
+            <button onClick={calculateRelationship} data-testid="calculate_relationship" >Calculate Relationship Future</button>
+            <button onClick={clearStatus} data-testid="clear" >Clear</button>
+
+            <h3 data-testid="answer" >{status}</h3>
         </div>
     )
-  }
 }
- 
- 
+
+
+
 export default App;
